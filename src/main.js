987f55dcac75e5d9465a66f87d51d37314017c10
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-15 22:50:06 +0700

    refactor: adjust code style

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
+
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 
+import '@/main.scss'
 import '@/plugins/vue-meta.plugin'
 import '@/registerPushNotification'
 import '@/registerServiceWorker'
-import '@/main.scss'
 
 Vue.config.productionTip = false
 
