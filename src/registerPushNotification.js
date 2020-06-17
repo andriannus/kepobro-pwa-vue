00698b8971f68a410348a6cbd3d7429c412d2e04
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-17 19:35:19 +0700

    refactor: show endpoint, p256dh key, and auth key

--- src/registerPushNotification.js
+++ src/registerPushNotification.js
@@ -2,7 +2,7 @@ import { PUBLIC_KEY } from '@/shared/constants/pn.constant'
 import { urlBase64ToUint8Array } from '@/shared/utils/transform.util'
 
 if ('Notification' in window) {
-  Notification.requestPermission().then((status) => {
+  Notification.requestPermission().then(status => {
     if (status === 'denied') {
       console.log('Notification denied')
       return
@@ -18,16 +18,44 @@ if ('Notification' in window) {
 }
 
 if ('PushManager' in window) {
-  navigator.serviceWorker.getRegistration().then((registration) => {
+  navigator.serviceWorker.getRegistration().then(registration => {
     const convertedPublicKey = urlBase64ToUint8Array(PUBLIC_KEY)
 
-    registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: convertedPublicKey
-    }).then(() => {
-      console.log('Push manager has been subscribed')
-    }).catch((error) => {
-      console.log('Error during push manager subscription: ', error)
-    })
+    registration.pushManager
+      .subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: convertedPublicKey
+      })
+      .then(subscribe => {
+        console.log('Push manager has been subscribed')
+
+        console.log(
+          'Successfully subscribed with endpoint: ',
+          subscribe.endpoint
+        )
+
+        console.log(
+          'Successfully subscribed with p256dh key: ',
+          btoa(
+            String.fromCharCode.apply(
+              null,
+              new Uint8Array(subscribe.getKey('p256dh'))
+            )
+          )
+        )
+
+        console.log(
+          'Successfully subscribed with auth key: ',
+          btoa(
+            String.fromCharCode.apply(
+              null,
+              new Uint8Array(subscribe.getKey('auth'))
+            )
+          )
+        )
+      })
+      .catch(error => {
+        console.log('Error during push manager subscription: ', error)
+      })
   })
 }
