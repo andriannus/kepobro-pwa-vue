79ba9d06cfe83036b99d23cbf1562c62ae7028c1
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-06-19 10:33:52 +0700

    refactor: change component scope

---
+++ src/shared/components/AppError.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="center-align u-marginTop-2">
+    <i class="material-icons large">build</i>
+    <p>
+      <strong>Waduh...</strong>
+      <br />Sepertinya ada kesalahan
+    </p>
+  </div>
+</template>
